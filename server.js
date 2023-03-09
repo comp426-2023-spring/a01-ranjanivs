e11687d774c8ef41e36409a0a29e11d34deb428d
Ranjani Venkatesh <ranjani@email.unc.edu>
2023-03-09 16:30:52 -0500

    fs module

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 const min = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
+const args = min(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const port = args.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -16,8 +16,13 @@ const min = require('minimist');
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
+readFile('./public/index.html', (err, data) => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+    
+});
 
 
 
